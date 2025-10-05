5b146192eb4bed504a872926dd76b30809a26d72
Tristan Santos <santostristan326@gmail.com>
2025-10-05 11:50:36 +0800

    'update'

--- naturebnb/src/App.tsx
+++ naturebnb/src/App.tsx
@@ -1,13 +1,9 @@
-import "./App.scss"
+import "./components/sass/App.scss"
 
 // import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 function App() {
-	return (
-		<>
-			<h1>Hello World</h1>
-		</>
-	)
+	return <></>
 }
 
 export default App
